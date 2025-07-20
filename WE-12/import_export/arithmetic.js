e6114d8981c1179974e153b88587ffb5de9c7332
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-07-20 13:56:08 +0530

    add:WE-12

---
+++ WE-12/import_export/arithmetic.js
@@ -0,0 +1,10 @@
+const sum = (a, b) => a + b
+
+const multiply = (a, b) => a * b
+
+const division = (a,b)=>a/b
+
+export default division //default export
+
+
+export { sum, multiply } //parameterised export
---
+++ WE-12/import_export/index.js
@@ -0,0 +1,7 @@
+import { sum, multiply } from './arithmetic.js' //parameterised import
+
+import division from './arithmetic.js'; //default import
+
+console.log(sum(1, 2));
+console.log(multiply(300, 400));
+console.log(division(200, 4));
